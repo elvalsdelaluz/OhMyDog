b01459bf0becde159d8d911541b6b37ac2cf9e90
tomasS2 <ilidtoto@gmail.com>
2023-05-20 18:05:57 -0300

    editar perro en adopcion hecho

--- ohmydog/adopcion/urls.py
+++ ohmydog/adopcion/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.adopcion, name="adopcion"),
     path('solicitud', views.publicacion, name="solicitud"),
     path('contactar/<int:adopcion_id>/', views.datos_adopcion, name="contactar"),
+    path('editar_informacion/<int:adopcion_id>/', views.editar_perro_adopcion, name="editar_informacion"),
 ]
