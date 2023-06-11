123beb20308d830d6941c80e32012dab2681d31c
Mariángeles <marialanus97@gmail.com>
2023-06-11 13:10:42 -0300

    Si el cliente autentica no tiene mascotas registradas y preciona INICIO no se va a redirigir a esa página. Falta hacer lo mismo con las otras c:

--- ohmydog/ohmydogapp/views.py
+++ ohmydog/ohmydogapp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from mascotas.models import Mascota
+from mascotas.views import alta_mascota
+from django.shortcuts import redirect
 
 def home (request):
-    
-    return render(request, "ohmydogapp/home.html")
+    usuario_autenticado = request.user
+    if usuario_autenticado.is_authenticated and not usuario_autenticado.is_staff:
+        #pregunto si tiene perros
+        tiene_perros = Mascota.objects.filter(dueño=usuario_autenticado).exists()
+        if not tiene_perros:
+             return redirect('alta_mascota/?valido2')
+        else:
+             return render(request, "ohmydogapp/home.html")  
+    else:
+        return render(request, "ohmydogapp/home.html")
 
