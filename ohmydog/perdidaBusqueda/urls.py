928851168b6ba62e66760c6ca7511d41241ad226
Mariángeles <marialanus97@gmail.com>
2023-06-25 14:13:55 -0300

    Cree las vistas publicar y mostrar perros perdidos

--- ohmydog/perdidaBusqueda/urls.py
+++ ohmydog/perdidaBusqueda/urls.py
@@ -4,5 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.perdidaBusqueda, name="perdidaBusqueda"),
+    path('', views.mostrar_perros_perdidos, name="mostrar_perros_perdidos"),
+    path('publicar_perro_perdido', views.publicar_perro_perdido, name="publicar_perro_perdido"),
 ]
